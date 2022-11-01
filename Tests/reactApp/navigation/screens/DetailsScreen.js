b4a34cfe4cb2fb693606c04342e1adc794d138ec
Phil768 <philippaul-2002@outlook.com>
2022-11-01 22:33:27 +0100

    y

--- Tests/reactApp/navigation/screens/DetailsScreen.js
+++ Tests/reactApp/navigation/screens/DetailsScreen.js
@@ -1,21 +1,55 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text, Dimensions, Button } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
 export default function DetailsScreen({ navigation }) {
+  const [mapRegion, setMapRegion] = useState({
+    latitude: 36.026666990852625,
+    longitude: 14.240446219546584,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const userLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission denied");
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true,
+    });
+    setMapRegion({
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+  };
+
+  useEffect(() => {
+    userLocation;
+  }, []);
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Text
-        onPress={() => navigation.navigate("Home")}
-        style={{ fontSize: 26, fontWeight: "bold" }}
-      >
-        Details Screen
-      </Text>
+    <View>
+      <MapView style={styles.map} region={mapRegion}>
+        <Button
+          title="Get current location"
+          onPress={userLocation}
+          style={styles.button}
+        />
+      </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+  button: {
+    border: "1px solid black",
+  },
+});
--- Tests/reactApp/navigation/screens/HomeScreen.js
+++ Tests/reactApp/navigation/screens/HomeScreen.js
@@ -1,18 +1,48 @@
-import * as React from 'react';
-import { View, Text} from 'react-native';
+import * as React from "react";
+import { View, Text } from "react-native";
+import { ListItem, Avatar } from "@react-native-material/core";
+import Icon from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function HomeScreen({ navigation }) {
-    return(
-        <View style = {
-            {
-                flex: 1, 
-                alignItems: 'center', 
-                justifyContent: 'center'
-            }
-        }> 
-        <Text onPress={() => alert("This is the home screen")} style={{fontSize: 26, fontWeight: 'bold'}}>
-            Home Screen
-        </Text>
-        </View>
-    );
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+      }}
+    >
+      <>
+        <ListItem
+          leadingMode="avatar"
+          leading={
+            <Avatar
+              image={{ uri: "https://mui.com/static/images/avatar/1.jpg" }}
+            />
+          }
+          title="Brunch this weekend?"
+          secondaryText="I'll be in your neighborhood doing errands this…"
+        />
+        <ListItem
+          leadingMode="avatar"
+          leading={
+            <Avatar
+              image={{ uri: "https://mui.com/static/images/avatar/2.jpg" }}
+            />
+          }
+          title="Summer BBQ"
+          secondaryText="Wish I could come, but I'm out of town this…"
+        />
+        <ListItem
+          leadingMode="avatar"
+          leading={
+            <Avatar
+              image={{ uri: "https://mui.com/static/images/avatar/3.jpg" }}
+            />
+          }
+          title="Oui Oui"
+          secondaryText="Do you have Paris recommendations? Have you ever…"
+        />
+      </>
+    </View>
+  );
 }
