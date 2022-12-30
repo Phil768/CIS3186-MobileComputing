80247021f91d7d6513c30ddee2f0a575c60c2006
Michael Vella <michael.vella.0110@gmail.com>
2022-12-30 17:15:22 +0100

    Adding car movement detection functionality

--- Mekkanik/Navigation/screens/HomeScreen.js
+++ Mekkanik/Navigation/screens/HomeScreen.js
@@ -1,18 +1,108 @@
 import * as React from "react";
-import { View, Text, TextInput, Button, StyleSheet } from "react-native";
+import { View, Text, TextInput, Button, StyleSheet, RefreshControl } from "react-native";
 import { collection, addDoc, query, where, getDocs } from "firebase/firestore";
 import { auth, db } from "../../configurations/index";
+import * as Location from 'expo-location';
+
 
 export default function HomeScreen(props) {
   // Creating a state which will hold the current 'car' object.
   const [car, setCar] = React.useState({});
-  // console.log('Passed props:', props);
+  const [kmYearDriven, setKmYearDriven] = React.useState(0);
+  const [kmMonthDriven, setKmMonthDriven] = React.useState(0);
+  const [kmDayDriven, setKmDayDriven] = React.useState(0);
+  const [carRuns, setCarRuns] = React.useState([]);
+  const currentUser = auth.currentUser;
+  const today = new Date();
+  const todaysDay = today.getDate();
+  const todaysMonth = today.getMonth() + 1;
+  const todaysYear = today.getFullYear();
+
+  const [currentLocation, setCurrentLocation] = React.useState(null);
+  const [previousLocation, setPreviousLocation] = React.useState(null);
+
+
+  React.useEffect(() => {
+    (async () => {
+      const location = await getCurrentLocation();
+      console.log(location);
+      setCurrentLocation(location);
+    })();
+  }, []);
+
+  React.useEffect(() => {
+    if (previousLocation && currentLocation) {
+      // Calculate the distance between the previous location and the current location
+      const distance = calculateDistance(previousLocation, currentLocation);
+
+      // If the distance is less than a certain threshold (e.g. 10 meters), we can assume the car has stopped
+      if (distance < 10) {
+        console.log('The car has stopped');
+      }
+    }
+  }, [currentLocation]);
+
+  React.useEffect(() => {
+    setPreviousLocation(currentLocation);
+  }, [currentLocation]);
+
+  async function getCurrentLocation() {
+    const { status } = await Location.requestBackgroundPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permission to access location was denied');
+      return;
+    }
+  
+    const location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true,
+    });
+  
+    return {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    };
+  }
+
+  function calculateDistance(loc1, loc2) {
+    // Calculate the distance between two locations using the Haversine formula
+    // More info: https://en.wikipedia.org/wiki/Haversine_formula
+    const R = 6371e3; // Earth's radius in meters
+    const lat1 = (loc1.latitude * Math.PI) / 180;
+    const lat2 = (loc2.latitude * Math.PI) / 180;
+    const deltaLat = ((loc2.latitude - loc1.latitude) * Math.PI) / 180;
+    const deltaLon = ((loc2.longitude - loc1.longitude) * Math.PI) / 180;
+  
+    const a =
+      Math.sin(deltaLat / 2) ** 2 +
+      Math.cos(lat1) * Math.cos(lat2) * Math.sin(deltaLon / 2) ** 2;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  
+    var totalDistanceTravelled = parseFloat(((R * c) / 1000).toFixed(2)); //KM
+
+    if (totalDistanceTravelled > 0){
+      console.log('Total distance travelled:', totalDistanceTravelled);
+      const docRef = addDoc(collection(db, "CarRuns"), {
+        carId: props.car.id,
+        day: todaysDay,
+        isActive: true,
+        kmDriven: totalDistanceTravelled,
+        month: todaysMonth,
+        year: todaysYear
+      });
+    }
+    
+    
+  }
+
+  // console.log('Todays day:', todaysDay);
+  // console.log('Todays month:', todaysMonth);
+  // console.log('Todays year:', todaysYear);
 
   // Get the document from the database
   const documentPath = 'Cars/' + props.car.id;
 
   // Getting the desired document
-  const getData = async () => {
+  const getCarData = async () => {
     const docRef = db.doc(documentPath);
     // console.log(docRef);
 
@@ -28,9 +118,66 @@ export default function HomeScreen(props) {
     });
   };
 
-  // Using a useEffect in order to get the data automatically once the page has loaded.
+  const getThisYearsCarRunsData = async() => {
+    let kmYearDriven = 0;
+    const query = db
+      .collection('CarRuns')
+      .where("carId", "==", props.car.id)
+      .where("year", "==", todaysYear)
+
+    const querySnapshot = await getDocs(query);
+
+    querySnapshot.forEach((documentSnapshot) => {
+      var data = documentSnapshot.data();
+      kmYearDriven = kmYearDriven + data.kmDriven;
+    });
+
+    setKmYearDriven(kmYearDriven);
+  };
+
+  const getThisMonthsCarRunsData = async() => {
+    let kmMonthDriven = 0;
+    const query = db
+      .collection('CarRuns')
+      .where("carId", "==", props.car.id)
+      .where("month", "==", todaysMonth)
+      .where("year", "==", todaysYear);
+
+    const querySnapshot = await getDocs(query);
+
+    querySnapshot.forEach((documentSnapshot) => {
+      var data = documentSnapshot.data();
+      kmMonthDriven = kmMonthDriven + data.kmDriven;
+    });
+
+    setKmMonthDriven(kmMonthDriven);
+  };
+
+  const getTodaysCarRunsData = async() => {
+    let kmDayDriven = 0;
+    const query = db
+      .collection('CarRuns')
+      .where("carId", "==", props.car.id)
+      .where("month", "==", todaysMonth)
+      .where("year", "==", todaysYear)
+      .where("day", "==", todaysDay);
+
+    const querySnapshot = await getDocs(query);
+
+    querySnapshot.forEach((documentSnapshot) => {
+      var data = documentSnapshot.data();
+      kmDayDriven = kmDayDriven + data.kmDriven;
+    });
+
+    setKmDayDriven(kmDayDriven);
+  };
+
+  // Using react's useEffect in order to get the data automatically once the page has loaded.
   React.useEffect(() => {
-    getData();
+    getCarData();
+    getThisYearsCarRunsData();
+    getThisMonthsCarRunsData();
+    getTodaysCarRunsData();
   });
 
   return (
@@ -40,8 +187,11 @@ export default function HomeScreen(props) {
         justifyContent: "center",
       }}
     >
-      <Text>{car.name},</Text>
+      <Text>{car.name}</Text>
       <Text>{car.email}</Text>
+      <Text>KM Driven (this year): {kmYearDriven}</Text>
+      <Text>KM Driven (this month): {kmMonthDriven}</Text>
+      <Text>KM Driven (today): {kmDayDriven}</Text>
     </View>
   );
 }
