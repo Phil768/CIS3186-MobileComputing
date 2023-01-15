4499639b5bab830fab4a51ae67bcd1142631a278
Matthew-Amaira <81741083+Matthew-Amaira@users.noreply.github.com>
2023-01-15 01:00:40 +0100

    Added Functionality to Maps

--- Mekkanik/Navigation/screens/MapScreen.js
+++ Mekkanik/Navigation/screens/MapScreen.js
@@ -1,15 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Text, Dimensions, Button } from "react-native";
-import MapView, { Marker } from "react-native-maps";
+import MapView, {PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import * as Location from "expo-location";
 //Main function of this screen.
 export default function MapScreen({ navigation }) {
+  markerState = ({myState:1});
+
   const [mapRegion, setMapRegion] = useState({
     latitude: 36.026666990852625,
     longitude: 14.240446219546584,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
+
+  const displayFuelPumps = async () => {
+    {
+      markerState.myState == 1?
+      <>
+      {markerState.setState({myState: 2})}
+      </>
+      :
+      <>
+      {markerState.setState({myState: 1})}
+      </>
+     
+    }
+  };
+
   //Getting the permission from the user to access their location.
   const userLocation = async () => {
     let { status } = await Location.requestForegroundPermissionsAsync();
@@ -36,8 +53,44 @@ export default function MapScreen({ navigation }) {
   //Returning the main body of the function to be displayed on screen.
   return (
     <View>
-      <MapView style={styles.map} region={mapRegion}></MapView>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map} 
+        region={mapRegion}
+      >
+        {
+          markerState.myState == 1?
+          <>
+          <Marker
+        coordinate={{
+        latitude: 35.9055,
+        longitude: 14.43335,
+        
+        }}
+      image={require('../../assets/mapmarker.png')}
+      title="Petrol Pump Mosta"
+      />
+
+      <Marker
+        coordinate={{
+        latitude: 35.9415,
+        longitude: 14.4113,
+        
+        }}
+      image={require('../../assets/mapmarker.png')}
+      title="Petrol Pump Burmarrad"
+      />
+      </>
+      : null
+      }
+      </MapView>
+
       <View style={{ position: "absolute", bottom: 200, left: 20 }}>
+      <Button
+          title="Display Near Fuel Stations"
+          onPress={displayFuelPumps}
+          style={styles.button}
+        />
         <Button
           title="Get current location"
           onPress={userLocation}
@@ -45,6 +98,7 @@ export default function MapScreen({ navigation }) {
         />
       </View>
     </View>
+    
   );
 }
 //Styling for this screen
